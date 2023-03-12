8425e4d8f07d5a7fd8c3c58891dd70036827a0ae
John Patrick <jpatrickzephyr@gmail.com>
2023-03-12 20:47:05 +0800

    remove unnecessary console logs

--- config/database.js
+++ config/database.js
@@ -8,7 +8,6 @@ const connectionParams = {
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@acc.3omz8gj.mongodb.net/user?retryWrites=true&w=majority`;
 
-console.log(uri);
 const connexion = mongoose
   .connect(uri, connectionParams)
   .then(() => console.log('Connected to User Database'))
