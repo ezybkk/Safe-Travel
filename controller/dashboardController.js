177be998723699e02b354cab5068a43e3d0970eb
Kyle <kychbardilas@gmail.com>
2023-05-10 15:02:18 +0800

    errors fixed

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -14,7 +14,7 @@ const getDashboard = async (req, res) => {
 
 const getHotline = async (req, res) => {
   const user = await req.user;
-  const hotlines = await Hotline.find({});
+  const hotlines = await Hotline.find({}).sort({Location: 1});
   res.render('dashboard/dashboardHotline', { user, hotlines });
 };
 
@@ -27,7 +27,7 @@ const getSpecificHotline = async (req, res) => {
 
 const getTouristSpot = async (req, res) => {
   const user = await req.user;
-  const spots = await Spot.find({});
+  const spots = await Spot.find({}).sort({ Location: 1, Name: 1});;
   res.render('dashboard/dashboardSpots', { user, spots });
 };
 
@@ -40,7 +40,7 @@ const getSpecificTouristSpot = async (req, res) => {
 
 const getRestaurant = async (req, res) => {
   const user = await req.user;
-  const restaurants = await Restaurant.find({});
+  const restaurants = await Restaurant.find({}).sort({ Location: 1, Name: 1});
   res.render('dashboard/dashboardRestaurant', { user, restaurants });
 };
 
@@ -52,9 +52,10 @@ const getSpecificRestaurant = async (req, res) => {
   res.render('dashboard/dashboardrestaurantSpecific', { restaurant, user });
 };
 
+
 const getHotel = async (req, res) => {
   const user = await req.user;
-  const hotels = await Hotel.find({});
+  const hotels = await Hotel.find({}).sort({ Location: 1, Name: 1 });
   res.render('dashboard/dashboardHotel', { user, hotels });
 };
 
@@ -71,108 +72,146 @@ const getProfile = async (req, res) => {
   res.render('dashboard/dashboardProfile', { user });
 };
 
+
+
+
+
 // const changeName = async (req, res) => {
-//   const { id } = user._id;
+//   const { id } = req.params;
 //   const { firstName, lastName } = req.body;
-//   const user = await req.user;
-//   await user.updateOne(
-//     { _id: id },
-//     {
-//       $push: {
-//         User: [
-//           {
-//             firstName: firstName,
-//             lastName: lastName,
-//           },
-//         ],
-//       },
+
+//   if (!firstName || !lastName) {
+//      req.flash('error', { message: 'First and last name is required.' });
+//      res.redirect('/dashboard/dashboardProfile');
+//   }
+
+//   else{
+//     try {
+//       const user = await User.findOneAndUpdate(
+//         { _id: id },
+//         { firstName, lastName },
+//       );
+//       res.redirect('/dashboard/dashboardProfile');
+//     } catch (error) {
+//       console.error(error);
+//       req.flash('error', { message: 'Error updating user' });
 //     }
-//   );
-//   res.redirect('/dashboard/dashboardProfile');
+//   }
 // }
 
-
-const changeName = async (req, res) =>{
+const changeName = async (req, res) => {
   const { id } = req.params;
   const { firstName, lastName } = req.body;
 
   try {
+    if (!firstName || !lastName) {
+      req.flash('error', 'First name and last name is required');
+      return res.redirect('/dashboard/dashboardProfile');
+    }
+
     const user = await User.findOneAndUpdate(
       { _id: id },
       { firstName, lastName },
       { new: true }
     );
+    req.flash('success', 'Name updated successfully');
     res.redirect('/dashboard/dashboardProfile');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error updating user');
+    req.flash('error', 'Error updating user');
+    res.redirect('/dashboard/dashboardProfile');
   }
 }
 
 
-const changePass = async(req, res) => {
+
+
+
+
+const changePass = async (req, res) => {
+  const { id } = req.params;
+  const { oldPassword, newPassword } = req.body;
+
   try {
-    const { id } = req.params;
-    const { oldPassword, newPassword } = req.body;
-  
-    // Fetch user from database
+    if (!oldPassword || !newPassword) {
+      req.flash('error', 'Current and new password is required');
+      return res.redirect('/dashboard/dashboardProfile');
+    }
+
     const user = await User.findById(id);
-  
+
     if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
+      req.flash('error', 'User not found');
+      return res.redirect('/dashboard/dashboardProfile');
     }
-  
-    // Check if the old password matches
-    if (!oldPassword) {
-      return res.status(400).json({ msg: 'Current password required' });
-    }
+
     const isMatch = await bcrypt.compare(oldPassword, user.password);
     if (!isMatch) {
-       return res.status(400).json({ msg: 'Invalid credentials' });
+      req.flash('error', 'Current password is incorrect');
+      return res.redirect('/dashboard/dashboardProfile');
     }
-  
-    // Hash the new password
-    if (!newPassword) {
-      return res.status(400).json({ msg: 'New password required' });
-    }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(newPassword, salt);
-  
-    // Update user's password in database
-    user.password = hashedPassword; // update the user object's password field
-    await user.save(); // save the updated user object to the database
-  
-    // res.json({ msg: 'Password updated successfully' });
+
+    user.password = hashedPassword;
+    await user.save();
+
+    req.flash('success', 'Password updated successfully');
     res.redirect('/dashboard/dashboardProfile');
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+  } catch (error) {
+    console.error(error);
+    req.flash('error', 'Error updating password');
+    res.redirect('/dashboard/dashboardProfile');
   }
 };
+
+
+
 
 
 // const changePass = async(req, res) => {
 //   try {
 //     const { id } = req.params;
-//     const user = await User.findById(req.user._id);
-
-//     // Check if current password matches
-//     await user.comparePassword(req.body.currentPassword);
-
-//     // Check if new password and confirm password match
-//     if (req.body.newPassword !== req.body.confirmPassword) {
-//       return res.status(400).send({ error: 'New password and confirm password do not match' });
+//     const { oldPassword, newPassword } = req.body;
+  
+//     // Fetch user from database
+//     const user = await User.findById(id);
+  
+//     if (!user) {
+//       return res.status(404).json({ msg: 'User not found' });
 //     }
-
-//     // Update user password
-//     user.password = req.body.newPassword;
-//     await user.save();
-
-//     res.send({ message: 'Password changed successfully' });
+  
+//     // Check if the old password matches
+//     if (!oldPassword) {
+//       return res.status(400).json({ msg: 'Current password required' });
+//     }
+//     const isMatch = await bcrypt.compare(oldPassword, user.password);
+//     if (!isMatch) {
+//        return res.status(400).json({ msg: 'Invalid credentials' });
+//     }
+  
+//     // Hash the new password
+//     if (!newPassword) {
+//       return res.status(400).json({ msg: 'New password required' });
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(newPassword, salt);
+  
+//     // Update user's password in database
+//     user.password = hashedPassword; // update the user object's password field
+//     await user.save(); // save the updated user object to the database
+  
+//     // res.json({ msg: 'Password updated successfully' });
+//     res.redirect('/dashboard/dashboardProfile');
 //   } catch (err) {
-//     res.status(400).send({ error: 'Unable to change password' });
+//     console.error(err.message);
+//     res.status(500).send('Server error');
 //   }
 // };
+
+
+
   
 
 
